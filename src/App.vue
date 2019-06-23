8f87733bce38c9993cc8dd51425acbbc1800cfd3
Kevin Müller <kevin.mueller1@live.de>
2019-06-23 15:09:04 +0200

    basic website with one category and one search term

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,153 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <v-app id="sandbox" :dark="dark">
+        <v-navigation-drawer class="header"
+                             v-model="primaryDrawer.model"
+                             :permanent="primaryDrawer.type === 'permanent'"
+                             :temporary="primaryDrawer.type === 'temporary'"
+                             :clipped="primaryDrawer.clipped"
+                             :floating="primaryDrawer.floating"
+                             :mini-variant="primaryDrawer.mini"
+                             absolute
+                             overflow
+                             app>
+            <v-toolbar flat>
+                <v-list>
+                    <v-list-tile>
+                        <v-list-tile-title class="title">
+                            Amazon Hot Searches
+                        </v-list-tile-title>
+                    </v-list-tile>
+                </v-list>
+            </v-toolbar>
+            <v-container>
+                <h2>Scheme</h2>
+                <v-switch v-model="dark" primary label="Dark"></v-switch>
+                <v-divider style="margin: 20px;"></v-divider>
+                <h2>Categories</h2>
+                <v-layout row wrap>
+                    <v-flex xs12>
+                        <v-checkbox v-for="(category) in categories" v-bind:key="category.name"
+                                    v-model="selectedCategories"
+                                    :label="category.name"
+                                    color="orange"
+                                    hide-details
+                                    :value="category.alias"
+                        >
+                        </v-checkbox>
+                        {{ selectedCategories.length }}
+                        {{ selectedCategories }}
+
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </v-navigation-drawer>
+        <v-toolbar class="header" :clipped-left="primaryDrawer.clipped" app absolute>
+            <v-toolbar-side-icon
+                    v-if="primaryDrawer.type !== 'permanent'"
+                    @click.stop="primaryDrawer.model = !primaryDrawer.model"
+            ></v-toolbar-side-icon>
+            <v-toolbar-title>Vuetify</v-toolbar-title>
+        </v-toolbar>
+        <v-content class="main">
+            <v-container fluid>
+
+                <v-layout row wrap v-if="this.results.length > 8">
+                    <v-flex v-for="res in results.slice(0, 8)" v-bind:key="res.text" xs3>
+                        <keyword-panel v-bind:keywords="results"></keyword-panel>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </v-content>
+        <v-footer :inset="footer.inset" app>
+            <span class="px-3">&copy; {{ new Date().getFullYear() }}</span>
+        </v-footer>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import KeywordPanel from './components/KeywordPanel'
+    import Vue from 'vue'
+    import Vuetify from 'vuetify'
+    import axios from "axios";
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    Vue.use(Vuetify)
+    export default {
+        name: 'App',
+        components: {
+            KeywordPanel: KeywordPanel
+        },
+        data: () => ({
+            results: [],
+            queries: ['a'],
+            selectedCategories: ['digital-text'],
+            categories: [
+                {
+                    name: 'Books',
+                    alias: 'digital-text',
+                    value: true
+                },
+                {
+                    name: 'Electronics',
+                    alias: 'electronics-intl-ship',
+                    value: false
+                }
+            ],
+            dark: true,
+            drawers:
+                ['Default (no property)', 'Permanent', 'Temporary'],
+            primaryDrawer: {
+                model: null,
+                type: 'temporary',
+                clipped: false,
+                floating: false,
+                mini: false
+            },
+            footer: {
+                inset: false
+            }
+        }),
+        mounted() {
+            axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+            axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
+            axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+            axios.defaults.baseURL = 'https://cors-anywhere.herokuapp.com/http://completion.amazon.com/search';
+
+            this.getHotSearches();
+        },
+
+        methods: {
+            getHotSearches() {
+                const urlBase = 'complete?client=amazon-search-ui&mkt=1';
+                this.selectedCategories.forEach(alias => {
+                    this.queries.forEach(query => {
+                        const url = urlBase + '&search-alias=' + alias + '&q=' + query;
+                        axios.get(url).then(response => {
+                            const data = response.data;
+                            if (data[0] === query && data.length > 1) {
+                                data[1].forEach(res => {
+                                    if (!this.results.includes(res)) {
+                                        this.results.push({
+                                            text: res,
+                                            category: alias,
+                                            color: Math.floor(Math.random() * 10)
+                                        })
+                                    }
+                                });
+                            }
+                        })
+                    });
+                });
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    .main {
+        background-color: #EAEDED;
+    }
+
+    .header {
+        background-color: #232F3F;
+    }
 </style>
---
+++ src/components/KeywordPanel.vue
@@ -0,0 +1,101 @@
+<template>
+    <v-container>
+        <v-card style="padding: 20px; min-height: 320px; max-height: 320px;"
+                :light="(keywords[keyword].color % colors.length) < 1"
+                :class="'mx-auto elevation-0 ' + colors[keywords[keyword].color % colors.length]" max-width="400">
+            <v-card-text style="min-height: 200px;" class="display-2 font-weight-bold">
+                {{ keywords[keyword].text.slice(0, typeWriterIndex) }}|
+            </v-card-text>
+
+            <v-card-actions>
+                <v-list-tile class="grow">
+                    <v-layout align-center>
+                        {{ keywords[keyword].category }}
+                    </v-layout>
+                    <v-layout align-center justify-end>
+                        <v-btn light :href="'https://www.amazon.com/s?k='+keywords[keyword].text"
+                               class="subheading gradient-grey">
+                            Seach!
+                        </v-btn>
+                    </v-layout>
+                </v-list-tile>
+            </v-card-actions>
+        </v-card>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            typeWriterIndex: 0,
+            typeWriterBaseSpeed: 200,
+            typeWriterSpeed: 200,
+            keyword: 0,
+            colors: ['white', 'mellowapricot', 'gunmetal', 'charcoal']
+        }),
+        props: {
+            keywords: Array,
+        },
+        created() {
+            this.refresh();
+            this.typeWriter();
+        },
+        methods: {
+            refresh() {
+                let length = 100;
+                if (this.typeWriterIndex === 0 || this.typeWriterIndex === this.keywords[this.keyword].text.length) {
+                    this.keyword = Math.floor(Math.random() * this.keywords.length);
+                    this.typeWriterIndex = 0;
+                }
+                if (this.typeWriterIndex === this.keywords[this.keyword].text.length - 1) {
+                    length = this.keywords[this.keyword].text.length * Math.max(Math.random() + 0.5, 1) * 400;
+                }
+                setTimeout(this.refresh, length);
+            },
+            typeWriter() {
+                const text = this.keywords[this.keyword].text;
+                if (this.typeWriterIndex < text.length) {
+                    this.typeWriterIndex++;
+                    this.typeWriterSpeed = this.typeWriterBaseSpeed + (Math.random() - 0.5) * 100;
+                }
+                setTimeout(this.typeWriter, this.typeWriterSpeed);
+            }
+        }
+    }
+</script>
+
+<style>
+    .gradient-no-switch {
+        flex: 1 1 auto;
+        transition: 0.5s;
+        background-size: 200% auto;
+    }
+
+    .gunmetal {
+        background-image: linear-gradient(to top, #232F3F 0%, #232F3F 51%, #232F3F 100%);
+        background-color: #232F3F;
+    }
+
+    .mellowapricot {
+        background-image: linear-gradient(to top, #FEBD69 0%, #FEBD69 51%, #FEBD69 100%);
+        background-color: #FEBD69;
+    }
+
+    .charcoal {
+        background-image: linear-gradient(to top, #37475A 0%, #37475A 51%, #37475A 100%);
+        background-color: #37475A;
+    }
+
+    .white {
+        background-image: linear-gradient(to top, #FFFFFF 0%, #FFFFFF 51%, #FFFFFF 100%);
+        background-color: #FFFFFF;
+    }
+
+    .gradient-yellow {
+        background-image: linear-gradient(to top, #EDBA36 0%, #F1C964 51%, #F5D588 100%);
+    }
+
+    .gradient-grey {
+        background-image: linear-gradient(to top, #DADDE2 0%, #EFF1F3 51%, #E6E9EF 100%);
+    }
+</style>
